acd297d35e3138f530613dbf2eaaab1585bb7508
tqdo <53948469+tqdo@users.noreply.github.com>
2022-04-16 07:10:55 -0400

    add holdings

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -1,5 +1,5 @@
 var link = 'https://f077-71-187-24-225.ngrok.io'
-// var link = 'https://finsheet.io'
+var link = 'https://finsheet.io'
 console.log(link)
 /*
  * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
