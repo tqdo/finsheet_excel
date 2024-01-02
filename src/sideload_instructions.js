cf1d6605390079fdfc5dc9d34db4b985a5ef71c1
tqdo <53948469+tqdo@users.noreply.github.com>
2024-01-02 10:52:50 -0500

    fix typo doc

--- src/sideload_instructions.js
+++ src/sideload_instructions.js
@@ -7,17 +7,17 @@
 
 
     <b>Test locally:</b>
-Close Excel
-rm -r ~/Library/Containers/com.microsoft.Excel
-sudo lsof -i :3000
-kill -9 7371
-Open Excel
-cd /Users/tuando/Library/Containers/com.microsoft.Excel/Data/Documents/
-mkdir wef
-cd wef
-open .
-(delete Production manifest if exists)
-    copy the file manifest.xml into wef folder (manifest runs files in localhost:3000)
+// Close Excel
+// rm -r ~/Library/Containers/com.microsoft.Excel
+// sudo lsof -i :3000
+// kill -9 7371
+// Open Excel
+// cd /Users/tuando/Library/Containers/com.microsoft.Excel/Data/Documents/
+// mkdir wef
+// cd wef
+// open .
+// (delete Production manifest if exists)
+//     copy the file manifest.xml into wef folder (manifest runs files in localhost:3000)
 CLose Excel
 npm run dev-server    // Run this in Golang
 Run in command line: defaults write com.microsoft.Excel OfficeWebAddinDeveloperExtras -bool true
